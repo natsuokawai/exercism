fe92377c923f130cb505d0a2d3228044a14f99cd
natsukawai <kawai.natsuo@gmail.com>
2019-12-13 09:28:46 +0900

    add c resister band

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,11 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t color) {
+    return color;
+}
+
+resistor_band_t* colors() {
+    static resistor_band_t colors[BAND_SIZE] = { BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE };
+
+    return colors;
+}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -2,3 +2,20 @@
 #define RESISTOR_COLOR_H
 
 #endif
+
+typedef enum {
+    BLACK,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE,
+    BAND_SIZE
+} resistor_band_t;
+
+int color_code(resistor_band_t color);
+resistor_band_t* colors();
