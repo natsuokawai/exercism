b60b9a2dba8ed94e4c5262c3d689f160c052e5e3
natsukawai <kawai.natsuo@gmail.com>
2019-12-17 09:23:02 +0900

    途中

--- c/word-count/src/word_count.c
+++ c/word-count/src/word_count.c
@@ -1,1 +1,31 @@
 #include "word_count.h"
+
+int word_count(const char *input_text, word_count_word_t * words) {
+  int i;
+  int count = 0;
+  char word[MAX_WORD_LENGTH];
+
+  while (*input_text) {
+    if (*input_text == ' ') {
+      *word = '\0';
+
+      if (count == 0) {
+        words[i].text = word;
+        words[i].count = 1;
+        count++;
+      } else {
+        for (i = 0; i < count; i++) {
+          if (*words->text == word) {
+            words[i].count += 1;
+            word = {};
+          } else {
+            words[i].text = word;
+            words[i].count = 1;
+          }
+        }
+      }
+    }
+
+    word++ = input_text++;
+  }
+}
