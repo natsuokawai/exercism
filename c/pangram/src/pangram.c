65dd1fe2e24edc2a9c209c70ff190b5e62d5992b
natsuokw <kawai.natsuo@gmail.com>
2020-01-02 11:24:38 +0900

    --amend

--- c/pangram/src/pangram.c
+++ c/pangram/src/pangram.c
@@ -8,48 +8,31 @@ void register_letters(const char *sentence, int *alphabets);
 bool check_alphabets(int *alphabets);
 
 bool is_pangram(const char *sentence) {
+    if (!sentence) return false;
+
+    bool result;
     int alphabets[ALPHABET_SIZE] = {false};
-    
+
     register_letters(sentence, alphabets); 
-    return check_alphabets(alphabets); 
+    result = check_alphabets(alphabets);
+
+    return result;
 }
 
 void register_letters(const char *sentence, int *alphabets) {
-    const char* dup = sentence;
-    while (!*dup) {
-       const char letter = tolower(*dup); 
-       if (letter >= 'a' && letter <= 'z') alphabets[(int)letter] = true; 
+    while (*sentence) {
+        const char letter = tolower(*sentence);
+        if (letter >= 'a' && letter <= 'z') {
+            alphabets[letter - 'a'] = true;
+        }
+        sentence++;
     }
 }
 
 bool check_alphabets(int *alphabets) {
     int i;
     for (i = 0; i < ALPHABET_SIZE; i++) {
-        if (alphabets[i] == false) return false;
+        if (!alphabets[i]) return false;
     }
     return true;
 }
-
-// #include <string.h>
-// #include <ctype.h>
-
-// #include "pangram.h"
-
-// bool is_pangram(const char *sentence) {
-//     if (sentence == NULL) return false;
-
-//     int seen[25] = {0};
-//     const char *tmp = sentence;
-//     while (*tmp != '\0') {
-//         const char curr = tolower(*tmp);
-//         if (curr >= 'a' && curr <= 'z') {
-//             seen[curr - 'a']++;
-//         }
-//         tmp++;
-//     }
-    
-//     for (int i = 0; i < 25; i++) {
-//         if (seen[i] == 0) return false;
-//     }
-//     return true;
-// }
