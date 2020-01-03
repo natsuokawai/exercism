ca45400b0f6f16b0318f8fe890ca46b03367f347
natsuokw <kawai.natsuo@gmail.com>
2020-01-03 18:41:12 +0900

    update: c grains

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -5,16 +5,14 @@
 
 uint64_t square(uint8_t index) {
     if (index == 0 || index > MAX_SQUARE_SIZE) return 0;
-    return pow(2, index - 1);
+    return 1ull << (index - 1);
 }
 
 uint64_t total(void) {
     int i;
-    int squares = 0;
     int result = 0;
     for (i = 0; i <= MAX_SQUARE_SIZE; i++) {
         result += square(i);
-        squares += i;
     }
     
     return result;
