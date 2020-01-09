ce6f64f0b2a6b73904977616bf5a9b86e1ee7953
natsukawai <kawai.natsuo@gmail.com>
2020-01-09 09:26:21 +0900

    update: c roman numerals

--- c/roman-numerals/src/roman_numerals.c
+++ c/roman-numerals/src/roman_numerals.c
@@ -1,22 +1,25 @@
 #include "roman_numerals.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAX_DIGITS 4
 
 char *to_roman_numeral(unsigned int number) {
     int tmp_num = (int)number;
     int num_list[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1,};
-    char roman_num_list[13][2] = {"M", "CM", "D", "CD", "C", "XC", "X", "IX", "IV", "I",};
-    static char result[50];
+    char roman_num_list[13][2] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I",};
+    int i = 0;
+    static char *result = malloc(sizeof(char) * 50);
 
     while (tmp_num) {
-        if (tmp_num - *num_list < 0) {
-            num_list++;
-            roman_num_list++;
+        if (tmp_num - num_list[i] < 0) {
+            i++;
             continue;
         } else {
-            tmp_num -= *num_list;
-            strcpy(result, *roman_num_list);
-            result += strlen(*roman_num_list);
+            printf("i: %d\n", i);
+            printf("tmp_num: %d, roman_num: %s\n", tmp_num, roman_num_list[i]);
+            strcpy(&result[strlen(result)], roman_num_list[i]);
+            tmp_num -= num_list[i];
         }
     }
 
